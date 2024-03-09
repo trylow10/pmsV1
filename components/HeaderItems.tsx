80fe671472296e16a861b217f8635a1104bb8bd1
sushant chapagain <sreddevil12@gmail.com>
2024-03-09 21:00:59 +0545

    fix removed unused files

--- components/HeaderItems.tsx
+++ components/HeaderItems.tsx
@@ -1,34 +1,12 @@
-import React from "react";
-import { Input } from "@/components/ui/input";
-import AvatarCustom from "./Avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import React from 'react';
+import { Input } from '@/components/ui/input';
+import { UserButton } from './auth/user-button';
 
 function Header() {
   return (
     <header className="flex items-center justify-end gap-6 border px-3 py-2 shadow-sm">
       <Input className="w-72" placeholder="search" />
-
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <AvatarCustom
-            src="https://randomuser.me/api/portraits/med/men/75.jpg"
-            username="sushant"
-          />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="mr-3">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Logout</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <UserButton />
     </header>
   );
 }
--- components/auth/user-button.tsx
+++ components/auth/user-button.tsx
@@ -1,21 +1,17 @@
-"use client";
+'use client';
 
-import { FaUser } from "react-icons/fa";
-import { ExitIcon } from "@radix-ui/react-icons"
+import { FaUser } from 'react-icons/fa';
+import { ExitIcon } from '@radix-ui/react-icons';
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  Avatar,
-  AvatarImage,
-  AvatarFallback,
-} from "@/components/ui/avatar";
-import { useCurrentUser } from "@/hooks/use-current-user";
-import { LogoutButton } from "@/components/auth/logout-button";
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { useCurrentUser } from '@/hooks/use-current-user';
+import { LogoutButton } from '@/components/auth/logout-button';
 
 export const UserButton = () => {
   const user = useCurrentUser();
@@ -24,13 +20,13 @@ export const UserButton = () => {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
-          <AvatarImage src={user?.image || ""} />
-          <AvatarFallback className="bg-sky-500">
+          <AvatarImage src={user?.image || ''} />
+          <AvatarFallback className="bg-teal-700">
             <FaUser className="text-white" />
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-40" align="end">
+      <DropdownMenuContent className="w-40 cursor-pointer" align="end">
         <LogoutButton>
           <DropdownMenuItem>
             <ExitIcon className="h-4 w-4 mr-2" />
