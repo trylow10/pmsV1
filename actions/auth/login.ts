5e1a4eebebea197a4aeb390b4578acfffb8bbc83
sushant chapagain <sreddevil12@gmail.com>
2024-03-11 15:58:29 +0545

    completed till menu handlers

--- actions/auth/login.ts
+++ actions/auth/login.ts
@@ -110,7 +110,6 @@ export const login = async (
     };
   } catch (error: any) {
     if (error) {
-      console.log(error);
       switch (error.type) {
         case 'CredentialsSignin':
           return { error: 'Invalid credentials!' };
