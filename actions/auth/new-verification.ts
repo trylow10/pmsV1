704ade41383db90ad11a1d1d0a22daa381dae8fc
trylow10 <aryaltrylowchan@gmail.com>
2024-03-14 14:22:03 +0545

    bug token error

--- actions/auth/new-verification.ts
+++ actions/auth/new-verification.ts
@@ -1,39 +1,39 @@
-"use server";
+'use server';
 
-import { db } from "@/lib/db";
-import { getUserByEmail } from "@/data/user";
-import { getVerificationTokenByToken } from "@/data/verificiation-token";
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
+import { getVerificationTokenByToken } from '@/data/verificiation-token';
 
 export const newVerification = async (token: string) => {
   const existingToken = await getVerificationTokenByToken(token);
-
+  //TODO: add a check for the token to see if it exists
   if (!existingToken) {
-    return { error: "Token does not exist!" };
+    return { error: 'Token does not exist!' };
   }
 
   const hasExpired = new Date(existingToken.expires) < new Date();
 
   if (hasExpired) {
-    return { error: "Token has expired!" };
+    return { error: 'Token has expired!' };
   }
 
   const existingUser = await getUserByEmail(existingToken.email);
 
   if (!existingUser) {
-    return { error: "Email does not exist!" };
+    return { error: 'Email does not exist!' };
   }
 
   await db.user.update({
     where: { id: existingUser.id },
-    data: { 
+    data: {
       emailVerified: new Date(),
       email: existingToken.email,
-    }
+    },
   });
 
   await db.verificationToken.delete({
-    where: { id: existingToken.id }
+    where: { id: existingToken.id },
   });
 
-  return { success: "Email verified!" };
+  return { success: 'Email verified!' };
 };
