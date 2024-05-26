b416144795ec361c1f9a06e213e4208bdd5f08df
sushantsursatech1 <sushant.chapagain@sursatech.com>
2024-05-26 23:05:47 +0545

    fix back btn on wrong side

--- app/(dashboard)/view-worker/[id]/page.tsx
+++ app/(dashboard)/view-worker/[id]/page.tsx
@@ -21,26 +21,24 @@ async function page({ params }: TParams) {
 
   if (!data)
     return (
-      <div className="flex flex-col items-start">
+      <>
         <BackButton />
         <Empty />
-      </div>
+      </>
     );
 
   const bundleLength = data.bundle.length;
 
   return (
     <>
-      <div className="flex justify-between">
-        <div>
-          <h3 className="font-semibold ">{data.name}</h3>
-          {bundleLength !== 0 && (
-            <span className="text-sm text-gray-500">
-              {bundleLength} {bundleLength === 1 ? 'record' : 'records'} found.
-            </span>
-          )}
-        </div>
+      <div>
         <BackButton />
+        <h3 className="font-semibold ">{data.name}</h3>
+        {bundleLength !== 0 && (
+          <span className="text-sm text-gray-500">
+            {bundleLength} {bundleLength === 1 ? 'bundle' : 'bundles'} assigned.
+          </span>
+        )}
       </div>
       <WorkerTable list={data?.bundle} />
     </>
